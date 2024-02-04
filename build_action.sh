b3454f9462e8540f66af9789a875384959d86e71
wanghui <ccwc1@163.com>
2024-02-04 23:10:37 +0800

    linux

---
+++ build_action.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+VERSION=$(grep 'Kernel Configuration' < config | awk '{print $3}')
+
+# add deb-src to sources.list
+sed -i "/deb-src/s/# //g" /etc/apt/sources.list
+
+# install dep
+apt update
+
+apt -y install libncurses5-dev build-essential openssl
+apt -u install  linux-image-generic 
+apt install -y wget xz-utils make gcc flex bison dpkg-dev bc rsync kmod cpio libssl-dev
+apt build-dep -y linux
+
+# change dir to workplace
+cd "${GITHUB_WORKSPACE}" || exit
+
+# download kernel source https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.7.3.tar.xz
+wget http://www.kernel.org/pub/linux/kernel/v6.x/linux-"$VERSION".tar.xz
+tar -xf linux-"$VERSION".tar.xz
+cd linux-"$VERSION" || exit
+
+# copy config file
+cp ../config .config
+
+# disable DEBUG_INFO to speedup build
+scripts/config --disable DEBUG_INFO
+
+# apply patches
+# shellcheck source=src/util.sh
+source ../patch.d/*.sh
+
+# build deb packages
+CPU_CORES=$(($(grep -c processor < /proc/cpuinfo)*2))
+make deb-pkg -j"$CPU_CORES"
+
+# move deb packages to artifact dir
+cd ..
+rm -rfv *dbg*.deb
+mkdir "artifact"
+mv ./*.deb artifact/
